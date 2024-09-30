4d3e0f070537414a157688ac42955af738b25e5a
MacriumBeta <vx@macrium.com>
2024-09-30 14:07:48 +0100

    Repository Moved

--- src/img_to_vhdx/arg_validator.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// Validates the command-line arguments.
-std::tuple<std::wstring, std::wstring, std::wstring, int, bool, bool, bool> handleCommandLineParameters(int argc, wchar_t* argv[]);
-
-// Converts a wide string to a UTF-8 string.
-std::string convertToUtf8(const std::wstring& wstr);
--- src/img_to_vhdx/vhdx_handler.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-// Function to handle VHDX file creation and mounting
-VHDXManager handleVHDXFile(const std::wstring& filename, const std::wstring& outputPath, std::wstring& vhdxName, const file_structs::fileLayout& backupFile, int diskNumber);
